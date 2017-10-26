8f7882fda8c134f7a5d4b20a5ed3bb77a98442e3
Joel Kraft <joelkraft3@yahoo.com>
2017-10-25 23:53:03 -0700

    pare back the templating code, send JSON repsonses

---
+++ README.md
@@ -0,0 +1,7 @@
+# Boilerplate for a REST api Express app
+
+To start, clone the repo, then in the project directory, type 
+
+```
+$ npm i && npm start
+```
--- app.js
+++ app.js
@@ -1,60 +1,55 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require("express");
+// morgan middleware gives feedback in the console
+var logger = require("morgan");
+// bodyParser allows you to parse request bodies.
+var bodyParser = require("body-parser");
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// These are route files being imported
+var routes = require("./routes/index");
+var users = require("./routes/users");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// Tell the app to use morgan middleware
+app.use(logger("dev"));
+// Tell the app to set any JSON bodies on the 
+// `response.body` obejct
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// Tell the app to check the main routes
+app.use("/", routes);
+// Tell the app to check the users routes
+app.use("/users", users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use((req, res, next) => {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+if (app.get("env") === "development") {
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: {}
+    });
 });
-
 
 module.exports = app;
